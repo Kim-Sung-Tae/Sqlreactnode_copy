293be33bdfea029525b41b6359f8cfce5a1fc5a1
kst <zyzuzizozz@naver.com>
2022-04-13 13:20:14 +0900

    퀵메뉴일러스트삽입

--- front/src/component/Quick.js
+++ front/src/component/Quick.js
@@ -3,14 +3,26 @@ import {Nav, NavItem,NavLink, } from 'reactstrap';
 
 
 
-
+var cnt =1; 
 export default function Navbar() {
+  function toggleImg(){ 
+    // document.getElementById("img").src="/img/m1.jpg";
+    var img1 = document.getElementById("img");
+      if(cnt%2==1){
+        img1.src="/img/Qafter.svg";
+        } else{
+          img1.src="/img/Qbefore.svg";
+        }
+        cnt++;
+  }
+  // 퀵메뉴 문이 클릭시마다 이미지 변경 하는 자바스크립트 !!!!!
+
+
   return (
 <div id ="quick">
-  
   <div class="dropup">
-    <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
-      Quick
+    <button onClick={toggleImg} class="btn dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
+      <img id="img" src="/img/Qbefore.svg" alt="quick메뉴"/>
     </button>
     <ul class="dropdown-menu quickbox" aria-labelledby="dropdownMenuButton1">
       <li><a class="kakao dropdown-item" href="#"></a></li>
